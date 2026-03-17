0f2f25d8e83a2c04b86c31029dc1412f32878007
Diego Guerrero <dasg1905@gmail.com>
2026-03-17 14:38:45 -0700

    StackLinkedList implementation

---
+++ Data Structures/StackLinkedList.java
@@ -0,0 +1,45 @@
+public class StackLinkedList<T> {
+    public Node<T> head;
+    public int length;
+
+    public StackLinkedList() {
+        this.head = null;
+        length = 0;
+    }
+
+    // -------------------- INSERTS ------------------------
+    // Time complexity: O(n), it is always going to be O(n)
+    public void push(T data) {
+
+        Node<T> newNode = new Node<T>(data);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+    }
+    // -------------------- REMOVE -------------------------
+    public T pop(){
+        if (this.head == null){
+            return null;
+        }
+        //Remove head
+        
+            T returnData = this.head.data;
+            this.head = this.head.next;
+            this.length--;
+            return returnData;
+    }
+    public T peek() {
+        return this.head.data;
+    }
+    // -------------------- UTILS --------------------------
+    public boolean isEmpty(){
+        if (this.head == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public int getLength(){
+        return this.length;
+    }
+}
