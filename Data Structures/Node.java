2aec9d4bf70cf739af5bc43483c0ebbd55480d9d
Diego Guerrero <dasg1905@gmail.com>
2026-03-17 14:45:10 -0700

    Node Implementation

---
+++ Data Structures/Node.java
@@ -0,0 +1,15 @@
+public class Node<T> {
+
+    //Data
+    public T data;
+
+    //Link
+    public Node<T> next;
+
+    Node(T data){
+        this.data = data;
+        this.next = null; //Serving as end marker, absensce of any data
+        // this.prev = null; //For Doubly Linked List
+    }
+    
+}
