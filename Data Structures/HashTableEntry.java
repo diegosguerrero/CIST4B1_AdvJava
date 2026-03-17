f9a4e171d3bf13cbd708ee26a9491748d191714e
Diego Guerrero <dasg1905@gmail.com>
2026-03-17 14:48:12 -0700

    HashTableEntry for BasicHashTable Implementation

---
+++ Data Structures/HashTableEntry.java
@@ -0,0 +1,10 @@
+public class HashTableEntry {
+    String key;
+    Object value;
+
+    //Constructor
+    public HashTableEntry(String key, Object value){
+        this.key = key;
+        this.value = value;
+    }
+}
